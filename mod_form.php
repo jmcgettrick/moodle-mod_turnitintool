002f43b1eace47752c1b6e9c2f6b4fdc7222dca8
David Hatton <dhatton@iparadigms.com>
2015-11-24 15:43:20 +0000

    fixed dumb error where wrong string lib was used.

--- mod_form.php
+++ mod_form.php
@@ -131,7 +131,7 @@ class mod_turnitintool_mod_form extends moodleform_mod {
         turnitintool_modform_help_icon('reportgenspeed', 'reportgenspeed', 'turnitintool', $mform);
         $mform->setDefault('reportgenspeed', $CFG->turnitin_default_reportgenspeed);
 
-        $mform->addElement('html', html_writer::tag('div', get_string('genspeednote', 'turnitintooltwo'), array('class' => 'tii_genspeednote')));
+        $mform->addElement('html', html_writer::tag('div', get_string('genspeednote', 'turnitintool'), array('class' => 'tii_genspeednote')));
         
 
         $suboptions = array( 0 => get_string('norepository','turnitintool'), 1 => get_string('standardrepository','turnitintool'));
